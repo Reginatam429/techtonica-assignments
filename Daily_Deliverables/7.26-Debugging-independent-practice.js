ef63e13194fb7497df1e0df76a97587c209fd7a7
Regina <reginatam429@gmail.com>
2019-07-26 19:17:50 -0700

    debugging deliverable

---
+++ Daily_Deliverables/7.26-Debugging-independent-practice.js
@@ -0,0 +1,10 @@
+function checkNum (int){
+    if (int >= 20 && int <= 100){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log (checkNum(30));
+console.log (checkNum(150));
