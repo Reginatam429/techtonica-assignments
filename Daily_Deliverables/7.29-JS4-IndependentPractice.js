5d8cf193a1577328294c2dc8581f6ba64b62266a
Regina <reginatam429@gmail.com>
2019-07-29 13:45:59 -0700

    added part2 of assignment

--- Daily_Deliverables/7.29-JS4-IndependentPractice.js
+++ Daily_Deliverables/7.29-JS4-IndependentPractice.js
@@ -12,3 +12,13 @@ function printVacations(array){
     printVacations([['Tammy', 'Tahiti'], ['Erin', 'Banff, Alberta, Canada'], ['Janet', 'London']]);
 
 //Activity 2 - Vacation Choices
+function printVacations(array){
+
+    for (let i=0; i<array.length;i++){
+      for(let j=1;j<array[i].length;j++){
+        console.log(`${array[i][0]} wants to go to ${array[i][j].slice(0,-1)}, or ${array[i][j].slice(-1)}.`);
+      }
+    }
+    }
+    
+    printVacations([ ['Tammy', ['Tahiti', 'Bali', 'Hawaii']], ['Erin', ['Banff, Alberta, Canada', 'Iceland']], ['Janet', ['London', 'Hogwarts']]]);
