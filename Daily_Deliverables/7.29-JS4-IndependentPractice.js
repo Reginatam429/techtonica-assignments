dd2b978fa0d07a5e2f9f464c88002f473dd82a02
Regina <reginatam429@gmail.com>
2019-07-29 11:59:32 -0700

    upload JS4

---
+++ Daily_Deliverables/7.29-JS4-IndependentPractice.js
@@ -0,0 +1,14 @@
+//Activity 1 - Vacation Time!
+
+function printVacations(array){
+
+    for (let i=0; i<array.length;i++){
+      for(let j=1;j<array[i].length;j++){
+        console.log(`${array[i][0]} wants to go to ${array[i][j]}.`);
+      }
+    }
+    }
+    
+    printVacations([['Tammy', 'Tahiti'], ['Erin', 'Banff, Alberta, Canada'], ['Janet', 'London']]);
+
+//Activity 2 - Vacation Choices
