483f8dddaf8624764ab963d37c71b921735ddb13
Regina <reginatam429@gmail.com>
2019-07-30 14:40:52 -0700

    added js5 excercise

--- Daily_Deliverables/7.30-JS5-IndependentPractice.js
+++ Daily_Deliverables/7.30-JS5-IndependentPractice.js
@@ -155,5 +155,23 @@ switch(num){
 
 //Conditional Statements: Switch
 
-
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(true){
+      case "aeiou".includes(s[0]):
+        letter = "A";
+        break;
+      case "bcdfg".includes(s[0]):
+        letter = "B";
+        break;
+      case "hjklm".includes(s[0]):
+        letter = "C";
+        break;
+      case "npqrstvwxyz".includes(s[0]):
+        letter = "D";
+        break;
+    }
+    return letter;
+}
     
