05796f60a9e44aa0e62e2d13e0d805bde571f7aa
Regina <reginatam429@gmail.com>
2019-07-29 17:38:53 -0700

    working on alternate answer hackerrank

---
+++ Daily_Deliverables/7.29-Hackerrank-loops.js
@@ -0,0 +1,34 @@
+function vowelsAndConsonants(s) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let consonants = [];
+    for (let i = 0; i < s.length; i++) {
+        let flag = false;
+        for (let j = 0; j < vowels.length; j++) {
+            if (s[i] === vowels[j]) {
+                console.log(s[i]);
+                flag = true;
+            }
+        }
+        if (!flag) {
+            consonants.push(s[i]);
+
+        }
+    }
+    for (let k = 0; k < consonants.length; k++) {
+        console.log(consonants[k]);
+    }
+}
+//vowelsAndConsonants("javascriptloops");
+
+//alternate way of doing it:
+
+function vowelsAndConsonants(word){
+let vowels = ["a","e","i","o","u"];
+    for(let i=0;i<s.length;i++){
+        if (vowels.includes(word[i])){
+            
+        }
+
+    }
+
+}
