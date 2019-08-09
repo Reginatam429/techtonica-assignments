d9f5b7887f8780a954e34f5880d7978c10d2bb36
Regina <reginatam429@gmail.com>
2019-08-08 17:17:17 -0700

    updating

--- Daily_Deliverables/EventRecommender-OOPProject/event.js
+++ Daily_Deliverables/EventRecommender-OOPProject/event.js
@@ -1,4 +1,21 @@
-class EventRecommender() {
+class Event{
+    constructor(eventName,eventType,eventDate,eventPrice){
+    this.eventName = eventName;
+    this.eventType = eventType;
+    this.eventDate = eventDate;
+    this.eventPrice = eventPrice;
+    }
+}
+
+class User{
+    constructor(userName,userInterests){
+    this.userName = userName;
+    this.userInterests = userInterests;
+    this.userEvents = [];
+    }
+}
+
+class EventRecommender {
     constructor(events,users) {
     // All main properties should go here.
     
@@ -9,18 +26,23 @@ class EventRecommender() {
 
     addEvent() {
     // Adds a new Event to the System
+       let newEvent = new Event(eventName,eventType,eventDate,eventPrice);
+       this.events.push(newEvent);
     }
 
     addUser() {
     // Adds a new User to the System
+       let newUser = new User(userName,userInterests);
+       this.events.push(newUser);
     }
 
     saveUserEvent(){
     //Allow users to save events to a personal Events array.
     }
 
-    deleteUser() {
+    deleteUser(userName) {
     // Deletes a User from the system
+    delete User.userName;
     }
    
     deleteEvent() {
