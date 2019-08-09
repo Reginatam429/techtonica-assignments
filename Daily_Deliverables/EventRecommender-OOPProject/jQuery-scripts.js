e70ea32d6394e069ccdd8b14aae692706e0a1e4a
Regina <reginatam429@gmail.com>
2019-08-09 16:49:37 -0700

    update OOP project

---
+++ Daily_Deliverables/EventRecommender-OOPProject/jQuery-scripts.js
@@ -0,0 +1,13 @@
+//$(document).ready(function(){
+    //this is another way of writing the initial document ready which means to load the jquery only AFTER the rest of the code is done
+$(function() {
+
+    // $('#idlikebtn').on('click',function(){
+    //     //this here happen when you click the button
+    //     $(this).find('special').remove();
+    //     $(this).hide();
+    // });
+
+    //$(selector).action()
+    
+});
