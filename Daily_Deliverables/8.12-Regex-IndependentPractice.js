52346a57eae78ccd6d39ec96bb33a34dc27fbda9
Regina <reginatam429@gmail.com>
2019-08-14 17:29:46 -0700

    updating fixed regex assignment

---
+++ Daily_Deliverables/8.12-Regex-IndependentPractice.js
@@ -0,0 +1,31 @@
+// Independent Practice
+// Go through the tutorial at https://regexone.com. Do at least lessons 1-10. This isn't Javascript specific, but it will help you understand how to match text with regular expressions.
+// Write a function that takes a word and returns true if the word ends with tonica
+const tonica = (input) => {
+    return (/tonica$/.test(input))
+};
+
+console.log(tonica("techtonica"));
+console.log(tonica("techta"));;
+
+// Write a function expression that takes a string replaces all instances of symantec with semantic?
+
+const replaceSym = (input) => {
+    return input.replace(/symantec/, 'semantic')
+};
+
+console.log(replaceSym("I love symantec!"));
+
+// Write a function that takes a string and deletes all words that end in ing?
+const ingDelete = (input) => {
+    return input.replace(/\w+[i][n][g][^\w]/g,'')
+};
+
+console.log(ingDelete("I am loving and hating symantec!"));
+...console.
+// Write a function that takes a string and returns true if it is an email address? Compare your answer with your neighbor.
+// Write a function that takes a string containing names separated by commas like "Leah, Russell, Michelle" and returns an array of names, ["Leah", "Russell", "Michelle"]
+// Challenges
+// Write a regular expression that matches any participant name, but not any staff name
+// What about a regular expression for all even-length strings?
+// Do lessons 11+ on https://regexone.com
