1859f42c7a6f544ac2ae63fa44aa351ef3ea05c7
Regina <reginatam429@gmail.com>
2019-08-20 13:25:47 -0700

    added pokemon api

--- Daily_Deliverables/8.20-IntroToDatabases.js
+++ Daily_Deliverables/8.20-IntroToDatabases.js
@@ -11,3 +11,29 @@
     //3.How do I connect a database with my website?
 
 //Data Modeling Part 1: Single Tables
+
+// Independent Practice
+// Exercise 1.
+// Imagine you are creating a website to organize your personal recipe collection. You want to be able to:
+
+// See the recipe name and description
+// Filter by vegetarian or non-vegetarian recipes
+// Sort by date added to your collection
+// Filter by which meal of the day the recipe is for
+// Track how many times you've made each recipe
+// Find out which friend gave you this recipe (if any)
+// Make a data model for this app, and answer the following questions:
+
+// What is the entity?
+// What are the attributes of the entity?
+// What is the primary key of the entity?
+// What are the data types of the attributes?
+// Are there other ways you might model this data?
+// Exercise 2.
+// In your own words, define the following terms:
+
+// Entity
+// Attribute
+// Data type
+// Primary key
+// Schema
---
+++ Daily_Deliverables/PokemonAPI/pokemon.js
@@ -0,0 +1,45 @@
+  $(document).ready(function () {
+    getPokemonList();
+  
+    $('#pokeList').change(function(){
+      getSpecificPokemon(this.value);
+    })
+    
+  });
+  
+  const getPokemonList = () => {
+    var request = $.ajax('https://pokeapi.co/api/v2/pokemon/')
+      .done(function (response) {
+        // console.log(response);
+        const jsonResponse = response.results;
+        // console.log(jsonResponse);
+  
+        jsonResponse.forEach( pokemon => {
+          // console.log(pokemon.name);
+          const html = `<option value=${pokemon.name}>${pokemon.name}</option>`;
+          $('#pokeList').append(html);
+        });
+      })
+      .fail(function (response) {
+        console.log(response);
+      })
+      .always(function () {
+        console.log('DONE');
+      });
+  }
+  
+  const getSpecificPokemon = name => {
+    var request = $.ajax('https://pokeapi.co/api/v2/pokemon/' + name)
+    .done(function (response) {
+      const imgSource = response.sprites.front_default;
+  w  
+      const imgHtml = `<img src=${imgSource}>`; 
+      $('#pokemonImage').html(imgHtml);
+    })
+    .fail(function (response) {
+      console.log(response);
+    })
+    .always(function () {
+      console.log('DONE ', name );
+    });
+  }
