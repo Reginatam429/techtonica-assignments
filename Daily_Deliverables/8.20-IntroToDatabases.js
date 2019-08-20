2da26f36e65c3eb5431fefd55150bc701c36ca19
Regina <reginatam429@gmail.com>
2019-08-20 09:47:06 -0700

    databases

---
+++ Daily_Deliverables/8.20-IntroToDatabases.js
@@ -0,0 +1,13 @@
+// Independent practice
+// Write down an example of when you could use a database (other than the examples given in this lesson)
+    //you can use a database to keep track of patient information at a hospital, users on a social media platform, people at a company or school.
+// Write down 1-3 things you learned from the video
+    //1. Databases are a good way to organize your data that allows you to update and look up data easily.
+    //2. Entities
+    //3. Attributes
+// Write down 3 questions you have about databases
+    //1.What programs do we store databases on?
+    //2.What language do I use to communicate with databases?
+    //3.How do I connect a database with my website?
+
+//Data Modeling Part 1: Single Tables
