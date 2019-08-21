b55f0722dcc67d8b66a7144c44da39d0b7a0aa08
Regina <reginatam429@gmail.com>
2019-08-21 15:51:17 -0700

    working with node

---
+++ Daily_Deliverables/8.15-Node.js
@@ -0,0 +1,15 @@
+// Objectives
+// Participants will be able to:
+
+// Download and install Node.js - DONE
+// Run commands in the Node.js REPL
+// Explain non-blocking I/O
+// Explain (a)synchronous calls
+// Explain event-driven programming
+
+// Specific Things To Teach
+// The main features of Node (ie: what makes Node, Node?)
+// Blocking vs. non-blocking I/O
+// Synchronous vs. asynchronous calls
+// Why a developer might choose Node.js over other web frameworks
+// Well-known companies that use Node.js
---
+++ Daily_Deliverables/getting-started-with-node/apps.js
@@ -0,0 +1,1 @@
+console.log("I'm learning to use Node.js!");
