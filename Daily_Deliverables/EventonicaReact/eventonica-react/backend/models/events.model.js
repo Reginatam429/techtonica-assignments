576b8d0dc3b3089e546a2721c2712b4901b834b8
Regina <reginatam429@gmail.com>
2019-09-05 19:33:37 -0700

    completed backend

--- Daily_Deliverables/EventonicaReact/eventonica-react/backend/models/events.model.js
+++ Daily_Deliverables/EventonicaReact/eventonica-react/backend/models/events.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const eventsSchema = new Schema({
     title: { type: String, required: true, unique: true, trim: true, minlength: 3},
-    location: { type: String, required: true, unique: true, trim: true},
-    Date: { type: Date, required: true, unique: true, trim: true }
+    location: { type: String, required: true, trim: true},
+    date: { type: Date, required: true, trim: true }
 }, {
     timestamps: true,   
 });
--- Daily_Deliverables/EventonicaReact/eventonica-react/backend/routes/savedevents.js
+++ Daily_Deliverables/EventonicaReact/eventonica-react/backend/routes/savedevents.js
@@ -21,4 +21,29 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Savedevents.findById(req.params.id)
+    .then(savedevents => res.json(savedevents))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Savedevents.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Saved Event deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Savedevents.findById(req.params.id)
+        .then(savedevents => {
+            savedevents.username = req.body.username;
+            savedevents.title = req.body.title;
+
+            savedevents.save()
+                .then(() => res.json('Saved Event updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
