ac13cf34e5680c76cf94f9c386986a2fdc263f64
Regina <reginatam429@gmail.com>
2019-09-11 14:47:37 -0700

    added recursion hw

---
+++ Daily_Deliverables/9.10-Recursion.js
@@ -0,0 +1,7 @@
+// Independent Practice
+// Write a recursive function isPalindrome(aString) that returns true if aString is a palindrome. A palindrome is any string that is the same read forwards or backwards:
+
+// isPalindrome('racecar') -> true
+// isPalindrome('step on no pets') -> true
+// isPalindrome('a') -> true
+// isPalindrome('goat') -> false
