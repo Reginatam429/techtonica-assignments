4e64f7acea134a0e8266fd3399bdcd22b7ca365d
Regina <reginatam429@gmail.com>
2019-09-11 15:20:09 -0700

    added isPalindrome function

--- Daily_Deliverables/9.10-Recursion.js
+++ Daily_Deliverables/9.10-Recursion.js
@@ -5,3 +5,18 @@
 // isPalindrome('step on no pets') -> true
 // isPalindrome('a') -> true
 // isPalindrome('goat') -> false
+
+isPalindrome = (aString) => {
+    //base case #1
+    if(aString.length === 0 || aString.length === 1) return true;
+    //base case #2 - to determine if it is not a palindrome
+    if(aString[0] !== aString[aString.length -1]) return false;
+
+    return isPalindrome(aString.substr(1, aString.length -2))
+
+}
+
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('step on no pets'));
+console.log(isPalindrome('a'));
+console.log(isPalindrome('goat'));
